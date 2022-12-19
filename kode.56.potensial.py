097b93effc9fbad8cc4730c0f6fd4980862b2b2a
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-12-19 12:48:13 +0700

    Add files via upload

---
+++ kode.56.potensial.py
@@ -0,0 +1,16 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+m = int(input("Masukan Nilai Massa Benda (M) : "))
+g = int(input("Masukan Nilai Gravitasi (g): "))
+h = int(input("Masukan Nilai Ketinggian Benda (h): "))
+
+# Mengkonversi
+ep = m*g*h
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Hasil Energi Potensial (Ep) = ',ep)
+print('==========================================================')
